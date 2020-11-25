2bdb524d799ba566c7d3de02573aa56424925e84
Deepak pundir <sendit2deepak@gmail.com>
2020-11-25 11:44:56 +0530

    first commit

---
+++ hello-world/src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>My todolist</h2>
+        <ul>
+            
+        </ul>
+    </div>
+</template>
+
+<script>
+import Todo from './Todo';
+export default {
+    name:'Todos', 
+    components:{
+        Todo
+    },
+    props:[
+        "todos"
+    ]
+}
+</script>
+
+<style scoped>
+
+</style>
